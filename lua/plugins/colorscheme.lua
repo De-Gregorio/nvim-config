1aa0b33028d47ef28d6f8c36b08fd23d1562af78
Francesco Junior De Gregorio <frajrdegre@gmail.com>
2025-04-28 19:55:23 +0200

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    priority = 1000,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+  },
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    priority = 1000,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    name = "nightfox",
+    priority = 1000,
+  },
+}
